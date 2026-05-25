8a579142076b3b2977fc23e31a8a104aa417b561
Owusu <fullstackowusu@gmail.com>
2026-05-25 17:14:37 -0500

    RGB ppm image outputer

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <cstdint>
+#include <fstream>
 
 int main() {
 
@@ -7,21 +8,22 @@ int main() {
     int image_width = 256;
     int image_height = 256;
 
-    // Render
-
-    std::cout << "P3\n" << image_width <<  " " << image_height << "\n255\n";
+    std::ofstream output("image.ppm", std::ios::binary);
+    output << "P6\n" << image_width << " " << image_height << "\n255\n";
 
     for (int i = 0; i < image_height; i++) {
         for (int j = 0; j < image_width; j++) {
             auto r = double(j) / (image_width-1);
             auto g = double(i) / (image_height-1);
-            auto b = 0.0;
+            auto b = 0.25 + 0.75 * (1.0 - r) * (1.0 - g);
 
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
+            std::uint8_t ir = static_cast<std::uint8_t>(255.999 * r);
+            std::uint8_t ig = static_cast<std::uint8_t>(255.999 * g);
+            std::uint8_t ib = static_cast<std::uint8_t>(255.999 * b);
 
-            std::cout << ir << " " << ig << " " << ib << "\n";
+            output.write(reinterpret_cast<const char*>(&ir), 1);
+            output.write(reinterpret_cast<const char*>(&ig), 1);
+            output.write(reinterpret_cast<const char*>(&ib), 1);
         }
     }
 
